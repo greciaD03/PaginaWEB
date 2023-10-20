84061f67269a9bc6d84d3341fd49e37c1c66f0f6
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-10-20 13:10:52 -0600

    A2

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -37,11 +37,3 @@ class PostController extends AbstractController
         ]);
     }*/
 }
-
-$string = 'No coger vocales';
-    echo preg_match_all("/[^aeiou]/", $string, $matches); // 10
-// Filtrar vocales y espacios:
-    echo preg_match_all("/[^ aeiou]/", $string, $matches); // 8
-// Filtrar consonantes:
-    $string = "NO coger MAYUSCULAS solo minusculas"; // 23
-    echo preg_match_all("/[^A-Z]/", $string, $matches);
